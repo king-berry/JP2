61f2800b0d3ff5fe36d83d878cacd84c1af2f391
king-berry <111548775+king-berry@users.noreply.github.com>
2023-04-28 14:47:07 +0700

    jp2
    jp2

---
+++ student/Application.java
@@ -0,0 +1,8 @@
+package student;
+
+public class Application {
+    public static void main(String[] args) {
+        StudentManagementSystem test = new StudentManagementSystem();
+        test.run();
+    }
+}
